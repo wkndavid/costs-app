829bc53d032e37caea09c8ba9411b164c15db588
wkndavid <workingnown@gmail.com>
2023-11-01 16:40:57 -0300

    Relacionando Componentes

---
+++ src/components/Frases.js
@@ -0,0 +1,7 @@
+function Frases() {
+    return(
+     <div>Componente com frase</div>   
+    )
+}
+
+export default Frases
--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,10 +1,14 @@
+import Frases from "./Frases"
+
 function HelloWorld() {
     const url2 = 'https://placehold.co/300x400/rose/white'
 
     return(
         <div>
-            <h1>Meu primeiro componente...</h1>
+            <Frases />
+            <h1>componente...</h1>
             <p><img src={url2} alt='Imagem 300x400 rosa' /></p>
+            <Frases />
         </div>
     )
 }
